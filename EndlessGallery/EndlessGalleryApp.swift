d81c1a3612902125fc8ce1873ac8e3446ed28836
Maleeha <maleeha139@gmail.com>
2025-08-29 11:43:47 +0500

    Initial Commit

---
+++ EndlessGallery/EndlessGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EndlessGalleryApp.swift
+//  EndlessGallery
+//
+//  Created by apple on 29/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct EndlessGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
